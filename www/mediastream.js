ea3e4bb7056d0f7e08ac0adc3252ceeb21ab900a
Simon MacDonald <simon.macdonald@gmail.com>
2017-06-04 18:59:05 -0400

    :shirt: Fixing linter errors

--- www/mediastream.js
+++ www/mediastream.js
@@ -2,71 +2,67 @@
 /* globals window, Promise */
 
 var exec = cordova.require('cordova/exec'),
-    utils = cordova.require('cordova/utils');
-    flagConstraints = 0;
+    utils = cordova.require('cordova/utils'),
+    flagConstraints = 0,
     flagDevices = 0;
 
-   var mediaDevices = {};
-   var supportedConstraints = {
-   		'width' : true ,
-   		'height': true ,
-   		'aspectRatio' : true ,
-   		'frameRate': true ,
-   		'facingMode' : true ,
-   		'volume': true ,
-   		'sampleRate' : true ,
-   		'sampleSize': true ,
-   		'echoCancellation' : true ,
-   		'latency': true ,
-   		'channelCount' : true ,
-   		'deviceId': true ,
-   		'groupId': true 
+var mediaDevices = {};
+var supportedConstraints = {
+    'width' : true ,
+    'height': true ,
+    'aspectRatio' : true ,
+    'frameRate': true ,
+    'facingMode' : true ,
+    'volume': true ,
+    'sampleRate' : true ,
+    'sampleSize': true ,
+    'echoCancellation' : true ,
+    'latency': true ,
+    'channelCount' : true ,
+    'deviceId': true ,
+    'groupId': true
+};
 
-   	};
+mediaDevices.getSupportedConstraints = function() {
+    var success = function(constraints) {
+        console.log('constraints: ' + JSON.stringify(constraints));
+        supportedConstraints = constraints;
+    };
 
-   mediaDevices.getSupportedConstraints = function(){
-   	
-   	// assign new values returned from native ios ; until then default values returned
+    // assign new values returned from native ios ; until then default values returned
+	if(flagConstraints !== 0) {
+       exec(success, null, 'MediaStream', 'getSupportedConstraints', []);
+    }
 
-   		if(flagConstraints != 1)
-               {
-               exec(success, null, "MediaStream","getSupportedConstraints", []);
-               var success = function(constraints) {
-                console.log("constraints: " + JSON.stringify(constraints));
-               supportedConstraints = constraints;
-               }
-               }
-               return supportedConstraints;
-               flagConstraints = 1;
-               
-   };
+    flagConstraints = 1;
+    return supportedConstraints;
+};
 
-    mediaDevices.enumerateDevices = function() {
+mediaDevices.enumerateDevices = function() {
     var devices = {};
-    if(flagDevices != 0){
-    exec(success, null, "MediaStream","enumerateDevices", []);
-    var success = function(device){
-               console.log(device.devices);
-               devices = device.devices ;
-    }
-    }
-               return devices;
-               flagDevices = 1;
 
-   //	return new Promise(function(resolve,reject){
+    var success = function(device) {
+       console.log(device.devices);
+       devices = device.devices ;
+    };
+
+    if(flagDevices !== 0){
+        exec(success, null, 'MediaStream', 'enumerateDevices', []);
+    }
+
+    flagDevices = 1;
+    return devices;
+    //	return new Promise(function(resolve,reject){
+    // });
+};
+
+
+mediaDevices.getUserMedia = function(constraints) {
+    exec(null, null, 'MediaStream', 'getUserMedia', constraints);
+
+    //	return new Promise(function(resolve,reject){
 
     // });
-   };
-
-
-   mediaDevices.getUserMedia = function(constraints) {
-               
-    exec(null,null,"MediaStream", "getUserMedia", constraints);
-
-   //	return new Promise(function(resolve,reject){
-
-    // });
-   };
-
+};
 
 module.exports = mediaDevices;
